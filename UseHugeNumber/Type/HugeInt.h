043ff85821c6b130969f9756129283c5a48a7f37
Mathias-ktc <mathias.kontrec@student.junia.com>
2024-04-29 12:03:09 +0200

    addfile

--- UseHugeNumber/Type/HugeInt.h
+++ UseHugeNumber/Type/HugeInt.h
@@ -1,8 +1,23 @@
-//
-// Created by thoma on 29/04/2024.
-//
+#ifndef HUGE_INT
+#define HUGE_INT
 
-#ifndef TUTOGIT_HUGEINT_H
-#define TUTOGIT_HUGEINT_H
+#include "HugeUnsignedInt.h"
 
-#endif //TUTOGIT_HUGEINT_H
+typedef enum Sign {
+    PLUS, MINUS
+}Sign;
+
+typedef struct HugeInt {
+    HugeUnsignedInt* absoluteValue;
+    Sign sign;
+}HugeInt;
+
+extern HugeInt* createHugeInt            (void);
+extern HugeInt* createHugeIntFromString  (const char* digitString);
+extern HugeInt* createHugeIntFromHugeInt (const HugeInt* hugeInt, const unsigned int size);
+extern void     deleteHugeInt            (HugeInt* hugeInt);
+extern int      getHugeIntLength         (const HugeInt* hugeInt);
+extern HugeInt* simplifyHugeInt          (HugeInt* hugeInt);
+
+#endif // !HUGE_INT
+

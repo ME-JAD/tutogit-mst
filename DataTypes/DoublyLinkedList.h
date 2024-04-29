87b847da815794b811f6f63a3d542e6625bb62ad
Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
2024-04-29 11:15:32 +0200

    addfile (#8)

--- DataTypes/DoublyLinkedList.h
+++ DataTypes/DoublyLinkedList.h
@@ -1,8 +1,29 @@
-//
-// Created by melos on 29/04/2024.
-//
+#ifndef DOUBLY_LINKED_LIST
+#define DOUBLY_LINKED_LIST
 
-#ifndef TUTOGIT_DOUBLYLINKEDLIST_H
-#define TUTOGIT_DOUBLYLINKEDLIST_H
+typedef unsigned short Digit;
 
-#endif //TUTOGIT_DOUBLYLINKEDLIST_H
+typedef struct Node {
+    Digit digit;
+    struct Node* next;
+    struct Node* previous;
+} Node;
+
+typedef struct DoublyLinkedList {
+    Node* start;
+    Node* end;
+} DoublyLinkedList;
+
+extern int               addDigitAtStart                  (DoublyLinkedList* doublyLinkedList, const Digit digit);
+extern int               addDigitAtEnd                    (DoublyLinkedList* doublyLinkedList, const Digit digit);
+extern int               isNodeEmpty                      (const Node* node);
+extern DoublyLinkedList* createDoublyLinkedList           (void);
+extern DoublyLinkedList* createDoublyLinkedListFromString (const char* digitString);
+extern char              digitToChar                      (const Digit digit);
+extern void              deleteDoublyLinkedList           (DoublyLinkedList* doublyLinkedList);
+extern int               getDoublyLinkedListLength        (const DoublyLinkedList* doublyLinkedList);
+extern void              removeStartFromDoublyLinkedList  (DoublyLinkedList* doublyLinkedList);
+extern void              removeEndFromDoublyLinkedList    (DoublyLinkedList* doublyLinkedList);
+extern Digit             getDigitByPlaceFromStart         (const DoublyLinkedList* doublyLinkedList, const unsigned int place);
+
+#endif // !DOUBLY_LINKED_LIST

178270c115009c2205df115f9f5d6c271d30bff5
Thomas-RabetVincent <118429807+Thomas-RabetVincent@users.noreply.github.com>
2024-04-29 11:55:07 +0200

    Dev show huge unsigned int (#19)
    * sdf (#7)
    
    * addfile (#8)
    
    * dev_showHugeUnsignedInt (#10)
    
    * sdf (#7)
    
    * addfile (#8)
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * Dev show huge unsigned int (#11)
    
    * sdf (#7)
    
    * addfile (#8)
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * dev_printHugeUnsignedInt (#12)
    
    * sdf
    
    * sdf
    
    * Dev show huge unsigned int (#13)
    
    * sdf (#7)
    
    * addfile (#8)
    
    * dev_printHugeUnsignedInt (#12)
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * Dev show huge unsigned int (#15)
    
    * sdf (#7)
    
    * addfile (#8)
    
    * dev_printHugeUnsignedInt (#12)
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * dev_printHugeUnsignedInt (#14)
    
    * sdf
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * Dev show huge unsigned int (#16)
    
    * sdf (#7)
    
    * addfile (#8)
    
    * dev_printHugeUnsignedInt (#12)
    
    * sdf
    
    * sdf
    
    * dev_printHugeUnsignedInt (#14)
    
    * sdf
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * dev_printHugeUnsignedInt (#17)
    
    * sdf
    
    * sdf
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    Co-authored-by: Thomas-RabetVincent <118429807+Thomas-RabetVincent@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -1,5 +1,40 @@
-//
-// Created by Mathias_ktc on 29/04/2024.
-//
-
 #include "ShowHugeNumber.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE_OF_EXPONENT_MARKUP 2
+#define EXPONENT_MARKUP " E"
+
+
+static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
+    const int stringLength = getHugeUnsignedIntLength (hugeUnsignedInt) + 1;
+    char* hugeUnsignedIntInString = NULL;
+
+    if (hugeUnsignedInt != NULL) {
+        hugeUnsignedIntInString = malloc (stringLength * sizeof (HugeUnsignedInt));
+        if (hugeUnsignedIntInString != NULL) {
+            Node* node = hugeUnsignedInt->start;;
+            for (int digitCounter = 0; (!isNodeEmpty (node)) && (digitCounter < stringLength); digitCounter++) {
+                hugeUnsignedIntInString[digitCounter] = digitToChar (node->digit);
+                node = node->next;
+            }
+            hugeUnsignedIntInString[stringLength - 1] = '\0';
+        }
+    }
+
+    return hugeUnsignedIntInString;
+}
+
+
+
+
+
+
+
+void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
+    char* hugeUnsignedIntString = HugeUnsignedIntToString (hugeUnsignedInt);
+    printf ("%s\n", hugeUnsignedIntString);
+    free (hugeUnsignedIntString);
+}
+

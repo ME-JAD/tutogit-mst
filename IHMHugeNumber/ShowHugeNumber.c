b8cfe5380d2f11c5a4b17dd89f734a069ff9bbf7
Thomas-RabetVincent <118429807+Thomas-RabetVincent@users.noreply.github.com>
2024-04-29 11:46:45 +0200

    Dev show huge unsigned int (#18)
    * sdf (#7)
    
    * addfile (#8)
    
    * dev_printHugeUnsignedInt (#12)
    
    * sdf
    
    * sdf
    
    * dev_printHugeUnsignedInt (#14)
    
    * sdf
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    
    * dev_printHugeUnsignedInt (#17)
    
    * sdf
    
    * sdf
    
    * sdf
    
    * sdf
    
    ---------
    
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>
    Co-authored-by: Thomas-RabetVincent <118429807+Thomas-RabetVincent@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
    Co-authored-by: Mathias-ktc <118429962+Mathias-ktc@users.noreply.github.com>

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -6,8 +6,35 @@
 #define SIZE_OF_EXPONENT_MARKUP 2
 #define EXPONENT_MARKUP " E"
 
+
+static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
+    const int stringLength = getHugeUnsignedIntLength (hugeUnsignedInt) + 1;
+    char* hugeUnsignedIntInString = NULL;
+
+    if (hugeUnsignedInt != NULL) {
+        hugeUnsignedIntInString = malloc (stringLength * sizeof (HugeUnsignedInt));
+        if (hugeUnsignedIntInString != NULL) {
+            Node* node = hugeUnsignedInt->start;;
+            for (int digitCounter = 0; (!isNodeEmpty (node)) && (digitCounter < stringLength); digitCounter++) {
+                hugeUnsignedIntInString[digitCounter] = digitToChar (node->digit);
+                node = node->next;
+            }
+            hugeUnsignedIntInString[stringLength - 1] = '\0';
+        }
+    }
+
+    return hugeUnsignedIntInString;
+}
+
+
+
+
+
+
+
 void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
     char* hugeUnsignedIntString = HugeUnsignedIntToString (hugeUnsignedInt);
     printf ("%s\n", hugeUnsignedIntString);
     free (hugeUnsignedIntString);
 }
+

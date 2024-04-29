b87be5c6aa7cf1825fb8e9214a8afa825c6bf63d
SalomePeyrard <118430080+SalomePeyrard@users.noreply.github.com>
2024-04-29 11:34:37 +0200

    sdf

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -1,3 +1,12 @@
+#include "ShowHugeNumber.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE_OF_EXPONENT_MARKUP 2
+#define EXPONENT_MARKUP " E"
+
+
 void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
     char* hugeUnsignedIntString = HugeUnsignedIntToString (hugeUnsignedInt);
     printf ("%s\n", hugeUnsignedIntString);

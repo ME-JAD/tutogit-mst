affe810f8626b73f8ee21ed0c050e2bf3dd80a8f
thoma <thomas.rabet-vincent@student.junia.com>
2024-04-29 11:59:32 +0200

    bonjour

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "DataTypes/DoublyLinkedList.h"
+#include "UseHugeNumber/Type/HugeUnsignedInt.h"
+#include "IHMHugeNumber/ShowHugeNumber.h"
 
-int main() {
-    printf("Hello, World! \n");
-    return 0;
+int main (void) {
+    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString
+            ("123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
+    hugeNumber = createHugeUnsignedIntFromString
+            ("0000000123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
 }
